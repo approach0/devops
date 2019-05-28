7ec3f117019518b5bb8a769545a897b95aad1a2c
t-k- <clock126@126.com>
2019-05-28 16:42:04 +0800

    a0 building job.

--- jobs/scripts/git-mirror.sh
+++ jobs/scripts/git-mirror.sh
@@ -4,15 +4,16 @@ cat << USAGE
 Description:
 Mirror a git repo from remote.
 Examples:
-$0 https://github.com/t-k-cloud/wsproxy ~/wsproxy master
+$0 https://github.com/t-k-cloud/wsproxy /root/wsproxy master
 USAGE
 exit
 fi
 
-[ $# -ne 3 ] && echo 'bad arg.' && exit
+[ $# -lt 2 ] && echo 'bad arg.' && exit
 REPO=$1
 MDIR=$2
 BRANCH=$3
+[ -z $3 ] && BRANCH=master
 
 if [ ! -e "$MDIR" ]; then
 	echo "$MDIR not exits, cloning..."
