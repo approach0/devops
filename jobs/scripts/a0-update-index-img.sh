a58ad73577daef5739221ad299b0ee4b9a6217b6
t-k- <clock126@126.com>
2019-05-29 13:33:11 +0800

    more safe order to update index image.

--- jobs/scripts/a0-update-index-img.sh
+++ jobs/scripts/a0-update-index-img.sh
@@ -1,6 +1,5 @@
 IMG="$1"
 RANK=$2
-RUN_CMD="$3"
 
 set -e # abort on any error
 
@@ -11,25 +10,23 @@ echo "make symbol link searchd into folder run-$RANK"
 mkdir -p ~/run-$RANK
 ln -sf ~/a0/searchd/run/searchd.out ~/run-$RANK/searchd.out
 
-if [ -e $IMG ]; then
-	echo "move index image $IMG ..."
-	mv $IMG ${IMG}.run
-fi
-
+# ensure nothing is using image device
 if mount | grep -q ${IMG}; then
-	echo "killing searchd cluster..."
-	[ -z "$(pidof searchd.out)" ] && killall -USR1 searchd.out && sleep 8
+	if [ ! -z "$(pidof searchd.out)" ]; then
+		echo "killing searchd cluster..."
+		killall -USR1 searchd.out
+		sleep 8
+	fi
 	echo "umount..."
 	umount mnt-${IMG}.run
 fi
 
+# then safely update image
+if [ -e $IMG ]; then
+	echo "update index image $IMG ..."
+	mv $IMG ${IMG}.run
+fi
+
+# image ready to be used after mounting
 echo "mounting index image..."
 ./vdisk-mount.sh reiserfs ${IMG}.run
-
-if [ $RANK -eq 0 ]; then
-	echo "create searchd tmux session..."
-	echo "$RUN_CMD"
-	tmux new -d -s 'searchd' "$RUN_CMD"
-	sleep 8
-	tmux list-sessions
-fi
