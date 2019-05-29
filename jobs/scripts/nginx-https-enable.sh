d709c5c17b8b14f7b5aa789950cf41f54ea1b42c
t-k- <clock126@126.com>
2019-05-29 17:47:17 +0800

    redirect http to https if https is enabled.

--- jobs/scripts/nginx-https-enable.sh
+++ jobs/scripts/nginx-https-enable.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Because of the # charater, I cannot issue this simple line in *.jog.cfg
 sed -i 's/# ssl_certificate/ssl_certificate/g' /etc/nginx/sites-available/default
+sed -i 's/# return/return/g' /etc/nginx/sites-available/default
