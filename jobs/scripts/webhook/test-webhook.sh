45d084fccdfd1d4a175a06cd7b050b520f5bb7ac
t-k- <clock126@126.com>
2019-06-03 17:41:27 +0800

    minor changes.

--- jobs/scripts/webhook/test-webhook.sh
+++ jobs/scripts/webhook/test-webhook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 file="test.json"
-curl -v -H "Content-Type: application/json" -d @"${file}" "http://45.33.70.160/github-webhook.php"
+curl -v -H "Content-Type: application/json" -d @"${file}" "http://xitizu.com/github-webhook.php"
