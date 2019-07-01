b766b0df5f814708b60b0bcb0394ed57b7f94b6b
t-k- <clock126@126.com>
2019-07-01 20:21:21 +0800

    improve webhook-bait.php

--- jobs/scripts/webhook/webhook-bait.php
+++ jobs/scripts/webhook/webhook-bait.php
@@ -1,11 +1,22 @@
 <?php
+header('Content-Type: text/plain');
+
 $req = file_get_contents("php://input");
 
 $json = json_decode($req, true);
 
 $req_pretty = json_encode($json, JSON_PRETTY_PRINT);
-$fp = fopen('bait.log', 'a');
+$fp = fopen('webhook.log', 'a');
+
+if ($fp === false) {
+        echo 'failed to write.';
+        http_response_code(500);
+        exit();
+}
+
 fwrite($fp, $req_pretty);
 fwrite($fp, "\n");
 fclose($fp);
+
+echo 'catch.';
 ?>
