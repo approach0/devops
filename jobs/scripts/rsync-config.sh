7ee406f5803bfe5a2fa0a049d07740da19d0927d
t-k- <clock126@126.com>
2019-12-07 21:50:08 +0800

    fix rsync-config.sh when rsyncd is aborted.

--- jobs/scripts/rsync-config.sh
+++ jobs/scripts/rsync-config.sh
@@ -7,6 +7,7 @@ sed -i 's/<ip>/${RSYNC_ALLOW_IP}/' rsyncd.conf
 sed -i 's/<port>/${RSYNC_PORT}/' rsyncd.conf
 if [ -e rsyncd.pid ]; then
 	kill -INT \$(cat rsyncd.pid)
+	rm -rf rsyncd.lock
 fi
 rsync --daemon --config=rsyncd.conf
 EOF
