ec30b26509499598a2b4e354d7dc9fb20709238d
t-k- <clock126@126.com>
2019-12-12 16:45:26 +0800

    do not rely on rsyncd.pid to kill rsync daemon.

--- jobs/scripts/rsync-config.sh
+++ jobs/scripts/rsync-config.sh
@@ -5,9 +5,9 @@ $SCP $CONFIG/rsyncd.conf $SSH_USER@$IP:rsyncd.conf
 $SSH << EOF
 sed -i 's/<ip>/${RSYNC_ALLOW_IP}/' rsyncd.conf
 sed -i 's/<port>/${RSYNC_PORT}/' rsyncd.conf
-if [ -e rsyncd.pid ]; then
-	kill -INT \$(cat rsyncd.pid)
-	rm -rf rsyncd.lock
-fi
+
+killall -INT rsync
+rm -rf rsyncd.lock
+
 rsync --daemon --config=rsyncd.conf
 EOF
